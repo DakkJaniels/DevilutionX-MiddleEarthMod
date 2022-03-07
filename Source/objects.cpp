f657f179cf8f49c16cfec70cb362dee44fa36934
DakkJaniels <6080734+DakkJaniels@users.noreply.github.com>
2022-03-07 16:50:29 -0500

    fix monster spawning on Level 16, fix barrel labeling (should all be urns)

--- Source/objects.cpp
+++ Source/objects.cpp
@@ -5446,12 +5446,8 @@ void GetObjectStr(const Object &object)
 		break;
 	case OBJ_BARREL:
 	case OBJ_BARRELEX:
-		if (currlevel >= 17 && currlevel <= 20)      // for hive levels
-			strcpy(infostr, _("Pod"));               // Then a barrel is called a pod
-		else if (currlevel >= 21 && currlevel <= 24) // for crypt levels
-			strcpy(infostr, _("Urn"));               // Then a barrel is called an urn
-		else
-			strcpy(infostr, _("Barrel"));
+		// Barrels replaced with Urns
+		strcpy(infostr, _("Urn"));
 		break;
 	case OBJ_SHRINEL:
 	case OBJ_SHRINER:
