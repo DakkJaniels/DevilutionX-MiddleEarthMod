ae74aeab6e7960b4338b595067836a6c187b7cd6
xzyxzy <xzyxzy@users.noreply.github.com>
2022-06-18 07:30:35 +0500

    look for new target after current one dies

--- Source/controls/plrctrls.cpp
+++ Source/controls/plrctrls.cpp
@@ -1624,7 +1624,7 @@ void plrctrls_after_check_curs_move()
 		if (pcursmonst == -1 && pcursobj == -1 && pcursitem == -1 && pcursinvitem == -1 && pcursstashitem == uint16_t(-1) && pcursplr == -1) {
 			FindTrigger();
 		}
-		return;
+		// return;
 	}
 
 	// Clear focuse set by cursor
