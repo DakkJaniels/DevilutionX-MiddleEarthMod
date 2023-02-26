c81b085765e8d71f70e9b1ed55bae7bcbcd2a731
DakkJaniels <6080734+DakkJaniels@users.noreply.github.com>
2023-02-26 11:18:26 -0500

    fix linux build, maybe

--- Packaging/nix/LinuxReleasePackaging.sh
+++ Packaging/nix/LinuxReleasePackaging.sh
@@ -1,10 +1,10 @@
 mkdir ./build/package
-find build/_CPack_Packages/Linux/7Z/ -name 'devilutionx' -exec cp "{}" ./build/devilutionx \;
-cp ./build/devilutionx ./build/package/devilutionx
-cp ./Packaging/resources/devilutionx.mpq ./build/package/devilutionx.mpq
-cp ./build/devilutionx*.deb ./build/package/devilutionx.deb
-cp ./build/devilutionx*.rpm ./build/package/devilutionx.rpm
+find build/_CPack_Packages/Linux/7Z/ -name 'middleearthmod' -exec cp "{}" ./build/devilutionx-me \;
+cp ./build/devilutionx-me ./build/package/devilutionx-me
+cp ./Packaging/resources/devilutionx-me.mpq ./build/package/devilutionx-me.mpq
+cp ./build/devilutionx*.deb ./build/package/devilutionx-me.deb
+cp ./build/devilutionx*.rpm ./build/package/devilutionx-me.rpm
 cp ./Packaging/nix/README.txt ./build/package/README.txt
 cp ./Packaging/resources/LICENSE.CC-BY.txt ./build/package/LICENSE.CC-BY.txt
 cp ./Packaging/resources/LICENSE.OFL.txt ./build/package/LICENSE.OFL.txt
-cd ./build/package/ && tar -cavf ../../devilutionx.tar.xz * && cd ../../
+cd ./build/package/ && tar -cavf ../../devilutionx-me.tar.xz * && cd ../../
