597f4482b1dfb283bd0cbfc42c33f2d0eb11659b
DakkJaniels <6080734+DakkJaniels@users.noreply.github.com>
2023-02-26 11:57:57 -0500

    fix appimage script. fix .dmg name

--- Packaging/nix/AppImage.sh
+++ Packaging/nix/AppImage.sh
@@ -3,4 +3,4 @@ mv build/AppDir/usr/share/diasurgical/devilutionx/devilutionx-me.mpq build/AppDi
 wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage -N
 chmod +x linuxdeploy-x86_64.AppImage
 ./linuxdeploy-x86_64.AppImage --appimage-extract-and-run --appdir=build/AppDir --custom-apprun=Packaging/nix/AppRun -d Packaging/nix/middleearthmod.desktop -o appimage
-mv middleearthmod*.AppImage middleearthmod.appimage
+mv DevilutionX*.AppImage middleearthmod.appimage
